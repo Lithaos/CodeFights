7a51ba33775baec9a9386825dd761aeca45cb624
Wojciech Raczyński <wojto3@op.pl>
2018-05-22 18:48:11 +0200

    Add files via upload

---
+++ 41 digitdegree/41 digitdegree.js
@@ -0,0 +1,21 @@
+function digitDegree(n) {
+    let count = 0;
+    if(n<=9){
+        return count;
+    }
+    else
+        {
+            while(true)
+                {
+                    count++;
+                    n=n.toString().split("").reduce((a,b)=>{
+                        return parseInt(a)+parseInt(b);
+                    });
+                    if(n<=9){
+                        break;
+                    }
+                }
+        }
+    return count;
+        }
+    
